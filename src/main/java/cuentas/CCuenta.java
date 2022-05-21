f698458cfcc16b087db27d8b785b9d7d4743e8da
Miguel <Miguel@10.0.2.15>
2022-05-21 17:50:07 +0200

    Carga inicial del proyecto

---
+++ src/main/java/cuentas/CCuenta.java
@@ -0,0 +1,112 @@
+package cuentas;
+
+/**
+ *
+ * @author Miguel Jesús Suárez Rivero
+ * @version 0.1 
+ * @since 2022
+ */
+
+public class CCuenta {
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+  /**
+   * 
+   * @param nom El nombre del titular de la cuenta
+   * @param cue La cuenta bancaria
+   * @param sal El saldo de la cuenta en double
+   * @param tipo El tipo de interés de la cuenta en double
+   */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    /**
+     * 
+     * @param cantidad El importe a ingresar en double
+     * @throws Exception Excepción lanzada al introducir cantidad incorrecta.
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+    /**
+     * @return el titular de la cuenta
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+    
+}
